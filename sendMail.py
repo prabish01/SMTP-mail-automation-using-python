cf39a6552143d182706f273a214121cb56fd62e2
Prabish <prabishdangi2002@gmail.com>
2023-11-30 23:16:42 +0545

    Automation with iteration inside csv

--- sendMail.py
+++ sendMail.py
@@ -1,60 +1,76 @@
-import smtplib
+import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from config import EMAIL, PASSWORD, RECEIVER_ID, SIGNATURE
+import pandas as pd
+
 
 # email credentials here
 
 sender_id = EMAIL
 sender_pass = PASSWORD
-receiver_id = RECEIVER_ID
+# receiver_id = RECEIVER_ID
 
 
-message = MIMEMultipart('alternative')
-message["Subject"] = "Automation test"
-message["From"] = sender_id
-message["To"] = receiver_id
+
 
 
 # our message here
 
-msg = """\
- Your text here
-"""
-html = f"""\
+# msg = """\
 
-{msg}
+# """
 
-<html>
-    <body>
-        Your Signature and other texts
+# Read csv file
+readfile = pd.read_csv("list.csv")
+
+for index, row in readfile.iterrows():
+    name = row["Name"]
+    room = row["Room no"]
+    email = row["Email"]
+    splitname = name.split()[0]
+
+    message = MIMEMultipart('alternative')
+    message["Subject"] = "Automation test 2"
+    message["From"] = sender_id
+
+    html = f"""\
+
+    <html>
+        <body>
+        <p> Congratulations <b> {splitname} </b>!
+        You have been selected for the automated testing of the system.
+        Classes will be started from 12-01-2023 onwards.
+        Your room is <b>{room}</b>
+    
+        Kindly enter your designated room.</p>
+            
         {SIGNATURE}
 
-    </body>
-</html>
+        </body>
+    </html>
 
 
-"""
+    """
 
 
 
-html_MSG = MIMEText(html,"html")
-message.attach(html_MSG)
+    html_MSG = MIMEText(html,"html")
+    message.attach(html_MSG)
 
 
-# connect to gmail server and send
+    # connect to gmail server and send
 
-try:
-    server = smtplib.SMTP("smtp.gmail.com",587)
-    server.starttls()
-    server.login(sender_id,sender_pass)
+    try:
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com",465, context = context) as server:
+            server.login(sender_id,sender_pass)
+            server.sendmail(sender_id,email,message.as_string())
+        print(f"Email sent successfully to {name} : {email} !!!")
+        
 
-    server.sendmail(sender_id,receiver_id,message.as_string())
-    print("Email sent successfully !!!")
-    
+    except Exception as e:
+        print (f"Something went wrong... Couldn't send mail to {name}: {email}",e)
 
-except Exception as e:
-    print ("Something went wrong...",e)
-
-finally:
-    server.quit()
+    # finally:
+    #     server.quit()
